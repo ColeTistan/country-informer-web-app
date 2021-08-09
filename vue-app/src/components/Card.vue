69b17983ddbd114c4dc8188966915b8222be911d
coletistan <coletistan@gmail.com>
2021-08-08 20:53:28 -0400

    rendered api data on vue card template

--- vue-app/src/components/Card.vue
+++ vue-app/src/components/Card.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="card">
-    <img src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="USA Flag">    
+  <div class="card" v-for="country of countries" :key="country">  
     <div class="card-content">
       <div class="card-header">
         <p class="country-name">
-            United States of America
+            {{ country.name }}
         </p>
-        <p class="country-alpha-code">
-            (USA)
-        </p>
+      </div>
+      <div class="card-img">
+        <img :src="country.flag" :alt="country.name">  
       </div>
       <div class="country-details">
         <ul>
-          <li>Captial: Washington D.C.</li>
-          <li>National Language: English</li>
-          <li>Region: Americas</li>
-          <li>Currency: United States dollar ($)</li>
+          <li>Captial: 
+            <span>{{country.capital}}</span>
+          </li>
+          <li>Region: 
+            <span>{{country.region}}</span>
+          </li>
+          <li>Subregion: 
+            <span>{{country.subregion}}</span>
+          </li>
+          <li>Population: 
+            <span>{{country.population}}</span>
+          </li>
         </ul>
       </div>
     </div>
@@ -23,9 +30,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
-}
+  data() {
+      return {
+        urlBase: 'https://restcountries.eu/rest/v2',
+        countries: []
+      }
+    },
+    methods: {
+      getAllCountries() {
+        axios.get(`${this.urlBase}/all`)
+          .then(res => {
+            this.countries = res.data
+            console.log(this.countries)
+          })
+        }
+      },
+    mounted() {
+      this.getAllCountries()
+    }
+  }
 </script>
 
 <style lang="scss">
@@ -36,28 +61,37 @@ export default {
   $dark-color: #0c2340;
   $white-color: #ffffff;
   $border: 1px solid $dark-color;
-  
+  $general-size: 1rem;
   .card {
     border: $border;
-    border-radius: 0 0 16px 16px;
-    margin: 1rem;
-      
-    img {
-      height: 12rem;
-      width: auto;
-      object-fit: cover;
-      width: auto;
-    }
+    border-radius: $general-size;
+    margin: $general-size;
   }
 
   .card-content {
-    padding: 1rem;
+    padding: $general-size;
 
     .card-header {
-      display: flex;
+      display: block;
       justify-content: space-between;
       align-items: center;
       font-size: 24px;
+
+      .country-name {
+        font-weight: 700;
+        text-align: center;
+        letter-spacing: 3px;
+      }
+    }
+
+    .card-img {
+      padding: 0.5rem;
+
+      img {
+        height: auto;
+        width: 15rem;
+        object-fit: cover;
+      }
     }
 
     .country-details {
@@ -66,6 +100,12 @@ export default {
       ul li {
         padding: 0.5rem;
         text-align: center;
+        letter-spacing: 1px;
+
+        span {
+          letter-spacing: 2px;
+          font-weight: 600;
+        }
       }
     }
   }
